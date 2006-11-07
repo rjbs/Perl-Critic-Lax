11990be11e8abf7a525927d1e09707ce5e0b2e89
Ricardo SIGNES <rjbs@codesimply.com>
2006-11-07 16:40:39 +0000

    r27631@249:  rjbs | 2006-11-07 10:34:42 -0500  more laxity  r27632@249:  rjbs | 2006-11-07 11:27:40 -0500  prep to release  r27633@249:  rjbs | 2006-11-07 11:33:22 -0500  tag release

---
+++ lib/Perl/Critic/Policy/Lax/RequireEndWithTrueConst.pm
@@ -0,0 +1,120 @@
+use strict;
+use warnings;
+
+package Perl::Critic::Policy::Lax::RequireEndWithTrueConst;
+
+=head1 NAME
+
+Perl::Critic::Policy::Lax::RequireEndWithTrueConst
+
+=head1 VERSION
+
+version 0.002
+
+=head1 DESCRIPTION
+
+This policy behaves like L<Perl::Critic::Policy::Modules::RequireEndWithOne>,
+but allows frivolity like ending with C<"Club sandwich">.
+
+The return value must be the final statement of the module.
+
+=head1 WARNINGS
+
+There are I<many> true values that this won't actually accept.  The biggest
+issue is returning lists or other comma-delimited values.  While it would be
+nice to support these, they're not the sort of club sandwich with which I
+usually end my code, so I'm not likely to code the fix myself.
+
+Patches welcome.
+
+=cut
+
+use Perl::Critic::Utils;
+use base qw(Perl::Critic::Policy);
+
+our $VERSION = '0.002';
+
+my $DESCRIPTION = q{Module does not end with true constant};
+my $EXPLANATION = q{Must end with a recognizable true value};
+
+sub default_severity { return $SEVERITY_HIGH  }
+sub default_themes   { return  }
+sub applies_to       { return 'PPI::Document' }
+
+sub violates {
+  my ($self, $elem, $doc) = @_;
+  return if is_script($doc); #Must be a library or module.
+
+  # Last statement should be a true constant.
+  my @significant = grep { _is_code($_) } $doc->schildren();
+  my $match = $significant[-1];
+  return if !$match;
+
+  return if $self->_is_true_enough($match);
+
+  # Must be a violation...
+  return $self->violation($DESCRIPTION, $EXPLANATION, $match);
+}
+
+sub _is_true_enough {
+  my ($self, $element) = @_;
+  
+  if ($element->isa('PPI::Statement::Break')) {
+    my ($head, @tail) = $element->schildren;
+    return unless $head eq 'return';
+    pop @tail if $tail[-1]->isa('PPI::Token::Structure')
+             and $tail[-1] eq ';';
+    $element = $tail[-1]; # If returning a list, only last one matters.
+  }
+
+  if ($element->isa('PPI::Statement') and $element->schildren < 3) {
+    ($element) = $element->schildren;
+  }
+
+  while ($element->isa('PPI::Structure::List')) {
+    my @list_elements = $element->schildren;
+    return unless @list_elements;
+    $element = $list_elements[-1];
+  }
+
+  if ($element->isa('PPI::Token::Number')) {
+    return $element ne '0'; # Any other number is true.
+  }
+
+  if ($element->isa('PPI::Token::Quote')) {
+    my $string = $element->string;
+    return((length $string) and ($string ne '0'));
+  }
+
+  # PPI::Statement::Expression for lists?  Probably too far to the edge.
+
+  return;
+}
+
+sub _is_code {
+  my $elem = shift;
+  return ! ($elem->isa('PPI::Statement::End')
+        ||  $elem->isa('PPI::Statement::Data'));
+}
+
+1;
+
+=pod
+
+=head1 AUTHOR
+
+
+Ricardo SIGNES <rjbs@cpan.org>
+
+Adapted from Module::RequireEndWithOne by Chris Dolan
+
+=head1 COPYRIGHT
+
+Copyright (c) 2006 Ricardo SIGNES, Chris Dolan, Jeffrey Ryan Thalhammer.  All
+rights reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
---
+++ lib/Perl/Critic/Policy/Lax/RequireExplicitPackage/ExceptForPragmata.pm
@@ -0,0 +1,119 @@
+use strict;
+use warnings;
+
+package Perl::Critic::Policy::Lax::RequireExplicitPackage::ExceptForPragmata;
+
+=head1 NAME
+
+Perl::Critic::Policy::Lax::RequireExplicitPackage::ExceptForPragmata;
+
+=head1 VERSION
+
+version 0.002
+
+=head1 DESCRIPTION
+
+This policy is meant to replace Modules::RequireExplicitPackage.  That policy's
+POD says:
+
+  In general, the first statement of any Perl module or library should be a
+  package statement.  Otherwise, all the code that comes before the package
+  statement is getting executed in the caller's package, and you have no idea
+  who that is.  Good encapsulation and common decency require your module to
+  keep its innards to itself.
+
+Sure, that's swell for code that has effect at a package level, but some
+statements are lexical.  This policy makes allowance for just one of those
+cases: turning on strictures, warnings, and diagnostics.
+
+This module understands the C<exempt_scripts> configuration parameter just like
+L<Perl::Critic::Policy::Modules::RequireExplicitPackage>.
+
+=cut
+
+use Perl::Critic::Utils;
+use base qw(Perl::Critic::Policy);
+
+our $VERSION = '0.002';
+
+my $EXPLANATION = 'Violates encapsulation';
+my $DESCRIPTION = 'Code (other than strict/warnings) not in explicit package';
+
+sub default_severity { return $SEVERITY_HIGH  }
+sub default_themes   { return qw( risky )     }
+sub applies_to       { return 'PPI::Document' }
+
+sub new {
+  my ($class, %args) = @_;
+  my $self = bless {}, $class;
+
+  #Set config, if defined
+  $self->{_exempt_scripts} =
+    defined $args{exempt_scripts} ? $args{exempt_scripts} : 1;
+
+  return $self;
+}
+
+# TODO: Make this configurable. -- rjbs, 2006-11-07
+my %allowed_pragmata = (
+  diagnostics => 1,
+  strict      => 1,
+  warnings    => 1,
+);
+
+sub violates {
+  my ($self, $elem, $doc) = @_;
+
+  # You can configure this policy to exclude scripts
+  return if $self->{_exempt_scripts} && is_script($doc);
+
+  # Find the first 'package' statement
+  my $package_stmnt = $doc->find_first('PPI::Statement::Package');
+  my $package_line = $package_stmnt ? $package_stmnt->location()->[0] : undef;
+
+  # Find all statements that aren't 'package' statements
+  my $stmnts_ref = $doc->find('PPI::Statement');
+  return if !$stmnts_ref;
+  my @non_packages =
+    grep { not(
+      $_->isa('PPI::Statement::Include') && $_->type eq 'use'
+      && exists $allowed_pragmata{ $_->module }
+    ) }
+    grep { !$_->isa('PPI::Statement::Package') } @{$stmnts_ref};
+  return if !@non_packages;
+
+  # If the 'package' statement is not defined, or the other
+  # statements appear before the 'package', then it violates.
+
+  my @viols = ();
+  for my $statement (@non_packages) {
+    my $statement_line = $statement->location->[0];
+    if ((not defined $package_line) || ($statement_line < $package_line)) {
+      push @viols, $self->violation($DESCRIPTION, $EXPLANATION, $statement);
+    }
+  }
+
+  return @viols;
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 AUTHOR
+
+Ricardo SIGNES <rjbs@cpan.org>
+
+Adapted from Modules::RequireExplicitPackage by Jeffrey Ryan Thalhammer.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2006 Ricardo SIGNES and Jeffrey Ryan Thalhammer.  All rights
+reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
