9fb6657cd2bce01b69b5758f0b539c3cb77a3c91
Ricardo SIGNES <rjbs@codesimply.com>
2006-11-07 03:30:35 +0000

    r27598@knight:  rjbs | 2006-11-06 22:14:30 -0500  I won't need that no critic thing with my new policy
     r27625@knight:  rjbs | 2006-11-06 22:28:31 -0500
     critic plugins
     r27626@knight:  rjbs | 2006-11-06 22:28:51 -0500
     more dist stuff
     r27627@knight:  rjbs | 2006-11-06 22:29:18 -0500
     structure
     r27628@knight:  rjbs | 2006-11-06 22:29:27 -0500
     tag release

---
+++ lib/Perl/Critic/Policy/Lax/ProhibitStringyEval/ExceptForRequire.pm
@@ -0,0 +1,112 @@
+package Perl::Critic::Policy::Lax::ProhibitStringyEval::ExceptForRequire;
+
+use strict;
+use warnings;
+use Perl::Critic::Utils;
+use base qw(Perl::Critic::Policy);
+
+our $VERSION = 0.001;
+
+my $DESCRIPTION = 'Expression form of "eval" for something other than require';
+my $EXPLANATION = <<'END_EXPLANATION';
+It's okay to use stringy eval to require a module by name, but otherwise it's
+probably a mistake.
+END_EXPLANATION
+
+sub default_severity { return $SEVERITY_HIGHEST  }
+sub default_themes   { return qw( danger )       }
+sub applies_to       { return 'PPI::Token::Word' }
+
+sub _arg_is_ok {
+  my ($self, $arg) = @_;
+
+  return unless $arg->isa('PPI::Token::Quote::Double')
+             or $arg->isa('PPI::Token::Quote::Interpolate');
+
+  my $string = $arg->string;
+
+  return unless my $doc = eval { PPI::Document->new(\$string) };
+
+  my @children = $doc->schildren;
+
+  # We only allow {require} and {require;number}
+  return if @children > 2;
+  return unless $children[0]->isa('PPI::Statement::Include');
+
+  # We could give up if the Include's second child isn't a Symbol, but... eh!
+
+  # So, we know it's got a require first.  If that's all, great.
+  return 1 if @children == 1;
+
+  # Otherwise, it must end in something like {1} or {1;}
+  return unless $children[1]->isa('PPI::Statement');
+
+  my @tail_bits = $children[1]->schildren;
+
+  return if @tail_bits > 2
+         or ! $tail_bits[0]->isa('PPI::Token::Number')
+         or ($tail_bits[1] && $tail_bits[1] ne ';');
+
+  return 1;
+}
+
+sub violates {
+  my ($self, $elem) = @_;
+
+  return if $elem ne 'eval';
+  return unless is_function_call($elem);
+
+  my $sib = $elem->snext_sibling();
+  return unless $sib;
+  my $arg = $sib->isa('PPI::Structure::List') ? $sib->schild(0) : $sib;
+
+  # Blocks are always just fine!
+  return if not($arg) or $arg->isa('PPI::Structure::Block');
+
+  # It's OK if the string we're evaluating is just "require $var"
+  # I should do THIS with PPI, too! -- rjbs, 2006-11-06
+  return if $self->_arg_is_ok($arg);
+
+  # Otherwise, you are in trouble.
+  return $self->violation($DESCRIPTION, $EXPLANATION, $elem);
+}
+
+1;
+
+=head1 NAME
+
+Perl::Critic::Policy::Lax::ProhibitStringyEval::ExceptForRequire
+
+=head1 DESCRIPTION
+
+Sure, everybody sane agrees that stringy C<eval> is usually a bad thing, but
+sometimes you need it, and you don't want to have to stick a C<no critic> on
+the end, because dangit, what you are doing is I<just not wrong>!
+
+See, C<require> is busted.  You can't pass it a variable containing the name of
+a module and have it look through C<@INC>.  That has lead to this common idiom:
+
+  eval q{ require $module } or die $@;
+
+This policy acts just like BuiltinFunctions::ProhibitStringyEval, but makes an
+exception when the content of the string is PPI-parseable Perl that looks
+something like this:
+
+  require $module
+  require $module[2];
+  use $module (); 1;
+
+=head1 AUTHOR
+
+Ricardo SIGNES <rjbs@cpan.org>
+
+Adapted from BuiltinFunctions::ProhibitStringyEval by Jeffrey Ryan Thalhammer
+
+=head1 COPYRIGHT
+
+This distribution is copyright 2006, Ricardo SIGNES.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
