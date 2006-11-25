c38eedb92c73f79c9076180f4e2c08060cf99c86
Ricardo SIGNES <rjbs@codesimply.com>
2006-11-25 13:42:24 +0000

    r28449@knight:  rjbs | 2006-11-25 08:40:41 -0500  release

---
+++ lib/Perl/Critic/Lax.pm
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+
+package Perl::Critic::Lax;
+
+=head1 NAME
+
+Perl::Critic::Lax - policies that let you slide on common exceptions
+
+=head1 VERSION
+
+version 0.005
+
+  $Id$
+
+=cut
+
+our $VERSION = '0.005';
+
+=head1 DESCRIPTION
+
+The Perl-Critic-Lax distribution includes versions of core Perl::Critic modules
+with built-in exceptions.  If you really like a Perl::Critic policy, but find
+that you often violate it in a specific way that seems pretty darn reasonable,
+maybe there's a Lax policy.  If there isn't, send one in!
+
+=head1 AUTHOR
+
+Ricardo SIGNES <rjbs@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2006 Ricardo SIGNES, Chris Dolan, Jeffrey Ryan Thalhammer.  All
+rights reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
+1;
