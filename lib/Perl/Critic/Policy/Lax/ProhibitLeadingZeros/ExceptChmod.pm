c44f8377b7caa13fc99404951d3da8e5176ad8e0
Ricardo Signes <rjbs@cpan.org>
2016-08-24 22:37:42 -0400

    no special code leaded to allow `chmod 0755` anymore

--- lib/Perl/Critic/Policy/Lax/ProhibitLeadingZeros/ExceptChmod.pm
+++ lib/Perl/Critic/Policy/Lax/ProhibitLeadingZeros/ExceptChmod.pm
@@ -5,47 +5,15 @@ package Perl::Critic::Policy::Lax::ProhibitLeadingZeros::ExceptChmod;
 
 =head1 DESCRIPTION
 
-This is a stupid mistake:
-
-  my $x = 1231;
-  my $y = 2345;
-  my $z = 0032;
-
-This is not:
-
-  chmod 0600, "secret_file.txt";
+This is subclass of
+L<Perl::Critic::Policy::ValuesAndExpressions::ProhibitLeadingZeros> with no
+changes.  It once allowed leading zeroes on numbers used as args to C<chmod>,
+but in 2008 the default Perl::Critic policy became to allow leading zeroes
+there and in a few other places.
 
 =cut
 
 use Perl::Critic::Utils;
-use parent qw(Perl::Critic::Policy);
-
-my $DESCRIPTION = q{Integer with leading zeros outside of chmod};
-my $EXPLANATION = "Only use leading zeros on numbers indicating file modes";
-
-sub default_severity { $SEVERITY_MEDIUM     }
-sub default_themes   { qw(lax bugs)         }
-sub applies_to       { 'PPI::Token::Number' }
-
-my $LEADING_ZERO_RE = qr{\A [+-]? (?: 0+ _* )+ [1-9]}mx;
-
-sub violates {
-  my ($self, $element, undef) = @_;
-
-  return unless $element =~ $LEADING_ZERO_RE;
-  return if $element->sprevious_sibling eq 'chmod';
-
-  my $working = eval { $element->parent->parent };
-  if ($element->parent->isa('PPI::Statement::Expression')) {
-    my $working = $element->parent->parent;
-    while (eval { $working->isa('PPI::Structure::List') }) {
-      $working = $working->parent;
-    }
-
-    return if $working and ($working->children)[0] eq 'chmod';
-  }
-
-  return $self->violation($DESCRIPTION, $EXPLANATION, $element);
-}
+use parent qw(Perl::Critic::Policy::ValuesAndExpressions::ProhibitLeadingZeros);
 
 1;
